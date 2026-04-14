5adbb34d4922a4ba758360b5dd7b98282ec83f1a
yeoshuheng <100367948+yeoshuheng@users.noreply.github.com>
2026-04-15 03:02:53 +0800

    feat: enable command to take in TTL

--- command/command.hpp
+++ command/command.hpp
@@ -5,7 +5,10 @@
 #ifndef COMMAND_HPP
 #define COMMAND_HPP
 
+#include <charconv>
+#include <format>
 #include <string>
+#include <utility>
 #include <vector>
 
 #include "../commons/utils.hpp"
@@ -19,6 +22,12 @@ struct PingCommand {};
 struct SetCommand {
     std::string key;
     storage::stored_value value;
+    uint32_t ttl_ms;
+
+    SetCommand(std::string key, storage::stored_value value)
+        : key(std::move(key)), value(std::move(value)), ttl_ms(0) {};
+    SetCommand(std::string key, storage::stored_value value, const uint32_t ttl_ms)
+        : key(std::move(key)), value(std::move(value)), ttl_ms(ttl_ms) {};
 };
 
 struct GetCommand {
@@ -52,8 +61,19 @@ inline Command build_command(ParsedResult& result) {
     }
     if (utils::fast_str_equals(cmd, "SET")) {
         if (arr.size() < 3) throw std::runtime_error("SET requires key + value");
-
-        return SetCommand{arr[1], arr[2]};
+        if (arr.size() > 4)
+            throw std::runtime_error("SET only takes in 3 arguments, key, value and ttl");
+        if (arr.size() == 3) {
+            return SetCommand(arr[1], arr[2]);
+        } else {
+            uint32_t ttl_ms;
+            if (auto [ptr, ec] =
+                    std::from_chars(arr[3].data(), arr[3].data() + arr[3].size(), ttl_ms);
+                ec != std::errc{}) {
+                throw std::runtime_error(std::format("failed to parse ttl, received: {}", arr[3]));
+            }
+            return SetCommand(arr[1], arr[2], ttl_ms);
+        }
     }
     throw std::runtime_error("unknown command: " + cmd);
 };
