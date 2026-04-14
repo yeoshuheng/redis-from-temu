3644fe8a6de4472ab210675182a5498d64dfe0cb
yeoshuheng <100367948+yeoshuheng@users.noreply.github.com>
2026-04-15 03:39:22 +0800

    feat: add core engine for adding data

---
+++ core/core.cpp
@@ -0,0 +1,86 @@
+//
+// Created by Yeo Shu Heng on 14/4/26.
+//
+
+#include "../core/core.hpp"
+
+#include <spdlog/spdlog.h>
+
+namespace core {
+RedisCore::RedisCore(io_ctx& ctx, const size_t max_capacity,
+                     const std::shared_ptr<channel>& in_channel,
+                     const std::shared_ptr<channel>& out_channel, const uint32_t poll_interval_ms,
+                     const uint32_t ttl_interval_ms)
+    : lru_cache(max_capacity),
+      max_capacity(max_capacity),
+      input(in_channel),
+      output(out_channel),
+      ctx(ctx),
+      poll_interval(poll_interval_ms),
+      ttl_interval(ttl_interval_ms),
+      poll_timer(ctx),
+      ttl_timer(ctx) {};
+
+void RedisCore::execute(command::Command& cmd) {
+    std::visit(
+        [this]<typename T>(const T& c) {
+            if constexpr (std::is_same_v<T, command::SetCommand>) {
+                lru_cache.add(c.key, c.value, c.ttl_ms);
+            } else if constexpr (std::is_same_v<T, command::DelCommand>) {
+                lru_cache.remove(c.key);
+            } else if constexpr (std::is_same_v<T, command::GetCommand>) {
+                lru_cache.get(c.key);
+            }
+        },
+        cmd);
+};
+
+boost::asio::awaitable<void> RedisCore::poll_loop() {
+    command::Command cmd;
+    while (is_running.load()) {
+        boost::system::error_code ec;
+        while (input->pop(cmd)) {
+            execute(cmd);
+        }
+        poll_timer.expires_after(std::chrono::milliseconds(poll_interval));
+        // instead of throwing error, redirect into an error_code for cleaner handling.
+        co_await poll_timer.async_wait(boost::asio::redirect_error(boost::asio::use_awaitable, ec));
+        if (ec) {
+            if (ec == boost::asio::error::operation_aborted) {
+                co_return;
+            }
+            spdlog::error("unexpected error in redis core poll loop: {}", ec);
+            co_return;
+        }
+    }
+};
+
+boost::asio::awaitable<void> RedisCore::ttl_loop() {
+    while (is_running.load()) {
+        boost::system::error_code ec;
+        lru_cache.remove_expired();
+        ttl_timer.expires_after(std::chrono::milliseconds(ttl_interval));
+        co_await ttl_timer.async_wait(boost::asio::redirect_error(boost::asio::use_awaitable, ec));
+        if (ec) {
+            if (ec == boost::asio::error::operation_aborted) {
+                co_return;
+            }
+            spdlog::error("unexpected error in redis core ttl loop: {}", ec);
+            co_return;
+        }
+    }
+};
+
+void RedisCore::start() {
+    is_running.store(true);
+    boost::asio::co_spawn(ctx, poll_loop(), boost::asio::detached);
+    boost::asio::co_spawn(ctx, ttl_loop(), boost::asio::detached);
+};
+
+void RedisCore::shutdown() {
+    is_running.store(false);
+    boost::system::error_code err;
+    poll_timer.cancel(err);
+    ttl_timer.cancel(err);
+};
+}  // namespace core
---
+++ core/core.hpp
@@ -0,0 +1,47 @@
+//
+// Created by Yeo Shu Heng on 14/4/26.
+//
+
+#ifndef CORE_HPP
+#define CORE_HPP
+
+#include <boost/asio.hpp>
+#include <boost/lockfree/spsc_queue.hpp>
+
+#include "../command/parser.hpp"
+#include "../storage/lru_cache.hpp"
+#include "../storage/object.hpp"
+
+namespace core {
+class RedisCore {
+    using channel = boost::lockfree::spsc_queue<command::Command>;
+    using io_ctx = boost::asio::io_context;
+    using timer = boost::asio::steady_timer;
+
+   private:
+    storage::LRUCache<std::string, storage::StoredObject> lru_cache;
+    size_t max_capacity;
+    std::shared_ptr<channel> input;
+    std::shared_ptr<channel> output;
+    std::atomic<bool> is_running{false};
+
+    io_ctx& ctx;
+    uint32_t poll_interval;
+    uint32_t ttl_interval;
+    timer poll_timer;
+    timer ttl_timer;
+
+    boost::asio::awaitable<void> poll_loop();
+    boost::asio::awaitable<void> ttl_loop();
+    void execute(command::Command& cmd);
+
+   public:
+    explicit RedisCore(io_ctx& ctx, size_t max_capacity, const std::shared_ptr<channel>& in_channel,
+                       const std::shared_ptr<channel>& out_channel, uint32_t poll_interval_ms,
+                       uint32_t ttl_interval_ms);
+    void start();
+    void shutdown();
+};
+}  // namespace core
+
+#endif  // CORE_HPP
