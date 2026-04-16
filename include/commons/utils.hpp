20727310ad84d499592647f7175ade80963ff6ba
yeoshuheng <100367948+yeoshuheng@users.noreply.github.com>
2026-04-16 15:49:49 +0800

    style: run linter

--- include/commons/utils.hpp
+++ include/commons/utils.hpp
@@ -8,6 +8,18 @@
 #include <string>
 
 namespace utils {
+
+inline uint32_t read_u32(const char*& ptr) {
+    uint32_t v;
+    std::memcpy(&v, ptr, sizeof(v));
+    ptr += sizeof(v);
+    return v;
+};
+
+inline void write_u32(std::string& out, const uint32_t v) {
+    out.append(reinterpret_cast<const char*>(&v), sizeof(v));
+};
+
 inline bool fast_str_equals(const std::string& s, const char* l) {
     // faster way to compare 2 strings agnostic to casing.
     size_t i = 0;
