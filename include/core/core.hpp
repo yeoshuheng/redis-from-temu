16ff1a01437266da8fd18d65653e2f2179de08ce
yeoshuheng <100367948+yeoshuheng@users.noreply.github.com>
2026-04-18 00:49:44 +0800

    feat: refactor rediscore to only handle r/w op to lru

--- include/core/core.hpp
+++ include/core/core.hpp
@@ -21,31 +21,18 @@ using io_ctx = boost::asio::io_context;
 using timer = boost::asio::steady_timer;
 using wal_ptr = std::shared_ptr<wal::WAL>;
 using run_atomic = std::atomic<bool>;
-class RedisCore final : commons::ThreadHeartBeat {
+class RedisCore final {
   private:
     RedisCache lru_cache;
     size_t max_capacity;
     wal_ptr wal;
-
-    run_atomic is_running{false};
-    io_ctx& ctx;
-    uint32_t poll_interval;
-    uint32_t ttl_interval;
     uint32_t ttl_budget;
-    timer poll_timer;
-    timer ttl_timer;
-    timer hb_timer;
-
-    boost::asio::awaitable<void> ttl_loop();
-    boost::asio::awaitable<void> beat_loop() override;
 
   public:
-    explicit RedisCore(io_ctx& ctx, size_t max_capacity, const commons::heartbeat_state& hb_state,
-        const wal_ptr& wal, uint32_t poll_interval_ms, uint32_t ttl_interval_ms,
-        uint32_t ttl_budget);
+    RedisCore(size_t max_capacity, const wal_ptr& wal, uint32_t ttl_budget);
+
     CoreResp execute(command::Command& cmd);
-    void start();
-    void shutdown();
+    void evict();
 };
 } // namespace core
 
--- src/core/core.cpp
+++ src/core/core.cpp
@@ -9,12 +9,8 @@
 #include <utility>
 
 namespace core {
-RedisCore::RedisCore(io_ctx& ctx, const size_t max_capacity,
-    const commons::heartbeat_state& hb_state, const wal_ptr& wal, const uint32_t poll_interval_ms,
-    const uint32_t ttl_interval_ms, const uint32_t ttl_budget)
-    : ThreadHeartBeat(hb_state), lru_cache(max_capacity), max_capacity(max_capacity), wal(wal),
-      ctx(ctx), poll_interval(poll_interval_ms), ttl_interval(ttl_interval_ms),
-      ttl_budget(ttl_budget), poll_timer(ctx), ttl_timer(ctx), hb_timer(ctx) {};
+RedisCore::RedisCore(const size_t max_capacity, const wal_ptr& wal, const uint32_t ttl_budget)
+    : lru_cache(max_capacity), max_capacity(max_capacity), wal(wal), ttl_budget(ttl_budget) {};
 
 CoreResp RedisCore::execute(command::Command& cmd) {
     return std::visit(
@@ -34,54 +30,12 @@ CoreResp RedisCore::execute(command::Command& cmd) {
                 }
                 return CoreResp{CoreResp::RespType::VALUE, v.value().val, "OK"};
             }
+            return CoreResp{CoreResp::RespType::ERROR, std::nullopt, "UNRECOGNIZED COMMAND"};
         },
         cmd);
 };
 
-boost::asio::awaitable<void> RedisCore::ttl_loop() {
-    while (is_running.load()) {
-        boost::system::error_code ec;
-        // budgeted ttl conviction to prevent hogging event loop from polling commands
-        lru_cache.remove_expired(ttl_budget);
-        ttl_timer.expires_after(std::chrono::milliseconds(ttl_interval));
-        co_await ttl_timer.async_wait(boost::asio::redirect_error(boost::asio::use_awaitable, ec));
-        if (ec) {
-            if (ec == boost::asio::error::operation_aborted) {
-                co_return;
-            }
-            spdlog::error("unexpected error in redis core ttl loop: {}", ec.message());
-            co_return;
-        }
-    }
-};
-
-void RedisCore::start() {
-    is_running.store(true);
-    boost::asio::co_spawn(ctx, ttl_loop(), boost::asio::detached);
-    boost::asio::co_spawn(ctx, beat_loop(), boost::asio::detached);
-};
-
-void RedisCore::shutdown() {
-    is_running.store(false);
-    boost::system::error_code err;
-    ttl_timer.cancel(err);
-    hb_timer.cancel(err);
-};
-
-boost::asio::awaitable<void> RedisCore::beat_loop() {
-    while (is_running.load()) {
-        boost::system::error_code ec;
-        hb_state->core_heartbeat.store(
-            Clock::now().time_since_epoch().count(), std::memory_order_relaxed);
-        hb_timer.expires_after(std::chrono::milliseconds(beat_interval_ms));
-        co_await hb_timer.async_wait(boost::asio::redirect_error(boost::asio::use_awaitable, ec));
-        if (ec) {
-            if (ec == boost::asio::error::operation_aborted) {
-                co_return;
-            }
-            spdlog::error("unexpected error in redis core heartbeat loop: {}", ec.message());
-            co_return;
-        }
-    }
+void RedisCore::evict() {
+    lru_cache.remove_expired(ttl_budget);
 };
 } // namespace core
